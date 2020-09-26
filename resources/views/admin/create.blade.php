d4848b7a989edc73358406433001681b26325cd4
Oliver <draper87@gmail.com>
2020-09-26 15:22:54 +0200

    Aggiunte mappe

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,3 +1,10 @@
+<head>
+    <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0"></script>
+    <script src="https://cdn.jsdelivr.net/algoliasearch/3.31/algoliasearchLite.min.js"></script>
+    <title></title>
+</head>
+
+
 <h1>Crea nuovo appartamento</h1>
 @if ($errors->any())
     <div class="alert alert-danger">
@@ -30,13 +37,13 @@
     <input type="file" name="image_path" placeholder="image_path" accept="image/*">
 
     <label for="address">address</label>
-    <input type="text" name="address" placeholder="address" value="{{ old('address')}}" required>
+    <input id="address" type="text" name="address" placeholder="address" value="{{ old('address')}}" required>
 
     <label for="longitude">Longitude</label>
-    <input type="text" name="longitude" placeholder="longitude" value="{{ old('longitude')}}" required>
+    <input id="lng-value" type="text" name="longitude" placeholder="longitude" value="{{ old('longitude')}}" required>
 
     <label for="latitude">Latitude</label>
-    <input type="text" name="latitude" placeholder="latitude" value="{{ old('latitude')}}" required>
+    <input id="lat-value" type="text" name="latitude" placeholder="latitude" value="{{ old('latitude')}}" required>
 
     @foreach ($facilities as $facility)
         <div>
@@ -47,3 +54,58 @@
 
     <input type="submit" value="Invia">
 </form>
+
+<script>
+    (function() {
+        var placesAutocomplete = places({
+            appId: 'plNO17G18F5R',
+            apiKey: '9bc42c41773997040e2daf6810f20401',
+            container: document.querySelector('#address')
+        });
+
+        placesAutocomplete.on('change', function(e) {
+            document.querySelector('#lat-value').value = e.suggestion.latlng.lat
+        });
+
+        placesAutocomplete.on('change', function(e) {
+            document.querySelector('#lng-value').value = e.suggestion.latlng.lng
+        });
+
+        placesAutocomplete.on('clear', function() {
+            document.querySelector('#lat-value').value = '';
+            document.querySelector('#lng-value').value = '';
+            document.querySelector('#address').value = '';
+        });
+
+        // parte relativa alla funzione locate me
+        placesAutocomplete.on('locate', function() {
+
+            var places = algoliasearch.initPlaces('plNO17G18F5R', '9bc42c41773997040e2daf6810f20401');
+
+            function updateForm(response) {
+                var hits = response.hits;
+                var suggestion = hits[0];
+
+                console.log(suggestion);
+
+                if (suggestion && suggestion.locale_names && suggestion.city) {
+                    placesAutocomplete.setVal(suggestion.locale_names.default[0] + ' ' + suggestion.city.default[0]);
+                }
+            }
+
+            navigator.geolocation.getCurrentPosition(function (response) {
+                var coords = response.coords;
+                lat = coords.latitude.toFixed(6);
+                lng = coords.longitude.toFixed(6);
+
+                places.reverse({
+                    aroundLatLng: lat + ',' + lng,
+                    hitsPerPage: 1
+                }).then(updateForm);
+            });
+
+
+        });
+
+    })();
+</script>
