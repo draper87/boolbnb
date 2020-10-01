5bd9918d1b7c6cfa459bdcf5f49ce3123bf61e74
Gianpiero <gpi91mx@gmail.com>
2020-10-02 00:33:47 +0200

    Basi modifiche style

--- public/js/app.js
+++ public/js/app.js
@@ -37395,8 +37395,8 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\drape\Desktop\Repo\boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\drape\Desktop\Repo\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Gianpiero\desktop\mamp\mamp-php\boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Gianpiero\desktop\mamp\mamp-php\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
