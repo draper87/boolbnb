4e21dd3a1dd7a32901d7d169c11255d0cb7a7599
Gianpiero <gpi91mx@gmail.com>
2020-10-11 23:20:24 +0200

    Inserimento pointer footer e link linkedin ai nomi home

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,27 +5,27 @@
     <ul class="list-unstyled list-inline text-center">
       <li class="list-inline-item">
         <a class="btn-floating btn-fb mx-1">
-          <i class="fab fa-facebook-f"> </i>
+          <i class="fab fa-facebook-f my_pointer"> </i>
         </a>
       </li>
       <li class="list-inline-item">
         <a class="btn-floating btn-tw mx-1">
-          <i class="fab fa-twitter"> </i>
+          <i class="fab fa-twitter my_pointer"> </i>
         </a>
       </li>
       <li class="list-inline-item">
         <a class="btn-floating btn-gplus mx-1">
-          <i class="fab fa-google-plus-g"> </i>
+          <i class="fab fa-google-plus-g my_pointer"> </i>
         </a>
       </li>
       <li class="list-inline-item">
         <a class="btn-floating btn-li mx-1">
-          <i class="fab fa-linkedin-in"> </i>
+          <i class="fab fa-linkedin-in my_pointer"> </i>
         </a>
       </li>
       <li class="list-inline-item">
         <a class="btn-floating btn-dribbble mx-1">
-          <i class="fab fa-dribbble"> </i>
+          <i class="fab fa-dribbble my_pointer"> </i>
         </a>
       </li>
     </ul>
